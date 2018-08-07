118f762cd3404f4644fa70044e7136101563ef94
ogiwara <ogiwara.dev@gmail.com>
2018-08-07 10:14:32 +0900

    add

--- GoF/GoF/AbstractFactory.cpp
+++ GoF/GoF/AbstractFactory.cpp
@@ -8,9 +8,51 @@
 
 #include "AbstractFactory.hpp"
 
-class Maze{}
-class Wall{}
-class Room{}
-class Door{}
+class Maze{};
+class Wall{};
+
+class Room{
+
+public:
+    Room(int number){
+        this->number = number;
+    }
+private:
+    int number;
+    
+};
+
+class Door{
+    
+public:
+    Door(Room *room1, Room *room2){
+        room = room1;
+    }
+    
+private:
+    Room *room;
+};
+
+class MazeFactory{
+
+public:
+    MazeFactory();
+    
+    virtual Maze* MakeMaze() const{
+        return new Maze();
+    }
+    
+    virtual Wall* MakeWall() const{
+        return new Wall();
+    }
+    
+    virtual Room* MakeRoom(int n) const{
+        return new Room(n);
+    }
+    
+    virtual Door* MakeDoor(Room *r1, Room *r2) const{
+        return new Door(r1, r2);
+    }
+};
 
 
